9ca17cbda50c5191aa6b41139bfb37d0e3673742
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-08-16 11:49:58 +0530

    final styling

--- form/forms.py
+++ form/forms.py
@@ -11,54 +11,65 @@ class add_data(forms.ModelForm):
             "name": TextInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Name",
                 }
             ),
             "domain": forms.Select(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:30px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 250px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:16px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Registration Number",
                 },
                 choices=(
                     ("Robotics", "Robotics"),
                     ("Creatives", "Creatives"),
                     ("Content", "Content"),
+                    ("Management", "Management"),
+                    ("Public Relations", "Public Relations"),
+                    ("Computer Vision", "Computer Vision"),
+                    ("Natural Language Processing", "Natural Language Processing"),
+                    (
+                        "Machine Learing & Deep Learning",
+                        "Machine Learing & Deep Learning",
+                    ),
+                    ("Cyber Security", "Cyber Security"),
+                    ("Web & App Development", "Web & App Development"),
+                    ("AR/VR", "AR/VR"),
                 ),
             ),
             "reg": TextInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Registration Number",
                 }
             ),
             "dept": TextInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Department",
                 }
             ),
             "email": EmailInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Email",
                 }
             ),
             "phone": NumberInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Phone",
                 }
             ),
             "reason": TextInput(
                 attrs={
                     "class": "form-control",
-                    "style": "max-width: 300px; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
+                    "style": "max-width: 600px; min-width:50vw; background:#3C3C3C; margin-bottom:10px; border:none; font-size:20px;color: white; opacity:0.8; padding:10px",
                     "placeholder": "Reason To Join",
                 }
             ),
--- form/models.py
+++ form/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
-domains = (("Robotics", "Robotics"), ("Creatives", "Creatives"), ("Content", "Content"))
+domains = (
+    ("Robotics", "Robotics"),
+    ("Creatives", "Creatives"),
+    ("Content", "Content"),
+    ("Management", "Management"),
+    ("Public Relations", "Public Relations"),
+    ("Computer Vision", "Computer Vision"),
+    ("Natural Language Processing", "Natural Language Processing"),
+    ("Machine Learing & Deep Learning", "Machine Learing & Deep Learning"),
+    ("Cyber Security", "Cyber Security"),
+    ("Web & App Development", "Web & App Development"),
+    ("AR/VR", "AR/VR"),
+)
 
 
 class Values(models.Model):
     name = models.TextField()
     reg = models.TextField()
     dept = models.TextField()
-    domain = models.CharField(max_length=20, choices=domains)
+    domain = models.CharField(max_length=40, choices=domains)
     email = models.TextField()
     phone = models.TextField()
     reason = models.TextField()
