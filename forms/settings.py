fb11ca89ff49b555c7db2ac517614926faee29ff
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-08-16 10:54:23 +0530

    styles all

--- forms/settings.py
+++ forms/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = "forms.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [[os.path.join(BASE_DIR, "templates")]],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -107,11 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
